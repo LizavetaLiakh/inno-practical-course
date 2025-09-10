8ae529ad1338be139bc088835d186ecf3102a386
LizavetaLiakh <lizaveta.liakh@innowise.com>
2025-09-10 20:45:53 +0300

    Add fairness and waiting

--- Skynet/src/main/java/com/innowise/skynet/Factory.java
+++ Skynet/src/main/java/com/innowise/skynet/Factory.java
@@ -5,18 +5,24 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Factory implements Runnable {
 
     private LinkedList<RobotPart> parts = new LinkedList<>();
+    private LinkedList<Faction> factions = new LinkedList<>();
     private volatile boolean isRunning = true;
     private LocalDateTime currentTime = LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT);
     private boolean isNight = false;
 
     private int dayCounter = 0;
     private final int DAYS_TOTAL;
+    private final static int PRODUCE_PLAN = 10;
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     /**
+     * Constructs a factory.
      *
      * @param days amount of days factory should work
      */
@@ -25,61 +31,92 @@ public class Factory implements Runnable {
     }
 
     /**
+     * Adds a new faction to steal parts from current factory.
+     *
+     * @param faction
+     */
+    public void addFaction(Faction faction) {
+        factions.add(faction);
+    }
+
+    /**
+     * Creates a list of robot parts for every faction that is linked with current factory.
      *
      * @param partsAmount amount of parts to steal
      * @return list of different stolen robot parts
      */
-    public synchronized List<RobotPart> stealParts(int partsAmount) {
-        List<RobotPart> stolenParts = new LinkedList<RobotPart>();
-        for (int i = 0; i < partsAmount && !parts.isEmpty(); i++) {
-            stolenParts.add(parts.removeFirst());
+    public List<RobotPart> stealParts(int partsAmount) {
+        lock.lock();
+        try {
+            List<RobotPart> stolenParts = new LinkedList<>();
+            for (int i = 0; i < partsAmount && !parts.isEmpty(); i++) {
+                stolenParts.add(parts.removeFirst());
+            }
+            return stolenParts;
+        } finally {
+            lock.unlock();
         }
-        return stolenParts;
     }
 
     /**
-     * Produces parts once a day
+     * Produces 10 parts once a day
      */
     public synchronized void produceParts() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < PRODUCE_PLAN; i++) {
             parts.add(RobotPart.getRandomPart());
         }
     }
 
     /**
+     * Returns a copy of the list of robot parts.
      *
-     * @return list of all produced robot parts
+     * @return copy of the list with all produced robot parts
      */
-    public LinkedList<RobotPart> getParts() {
-        return parts;
+    public LinkedList<RobotPart> getPartsCopy() {
+        return new LinkedList<>(parts);
     }
 
     @Override
     public void run() {
-        while (isRunning && dayCounter < DAYS_TOTAL) {
+        while (dayCounter < DAYS_TOTAL) {
             synchronized (this) {
                 currentTime = currentTime.with(LocalTime.of(10, 0));
                 isNight = false;
                 produceParts();
                 notifyAll();
+
+                waitSomeTime();
             }
 
             synchronized (this) {
                 currentTime = currentTime.plusDays(1).with(LocalTime.MIDNIGHT);
-                dayCounter++;
                 isNight = true;
                 notifyAll();
+
+                waitSomeTime();
+                dayCounter++;
             }
         }
 
-        isRunning = false;
-
         synchronized (this) {
+            isRunning = false;
             notifyAll();
         }
     }
 
     /**
+     * Makes factory wait some time at night/day.
+     */
+    private void waitSomeTime() {
+        try {
+            wait(50);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * Returns true if factory is running.
      *
      * @return true if thread is running
      */
@@ -88,10 +125,11 @@ public class Factory implements Runnable {
     }
 
     /**
+     * Returns true if it's night now.
      *
      * @return true if it's night now
      */
-    public boolean getIsNight() {
+    public synchronized boolean getIsNight() {
         return isNight;
     }
 
@@ -99,7 +137,7 @@ public class Factory implements Runnable {
      *
      * @return amount of days when factory worked
      */
-    public int getDayCounter() {
+    public synchronized int getDayCounter() {
         return dayCounter;
     }
 
@@ -110,4 +148,15 @@ public class Factory implements Runnable {
                 DAYS_TOTAL;
     }
 
+    public synchronized int getPartsCount() {
+        return parts.size();
+    }
+
+    public LocalDateTime getCurrentTime() {
+        return currentTime;
+    }
+
+    public int getDaysTotal() {
+        return DAYS_TOTAL;
+    }
 }
